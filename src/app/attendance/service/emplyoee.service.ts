bdb80380bd895cf6f938a68cf84b934f104299cd
peehu sharma <videveloper101@yahoo.com>
2026-06-09 18:57:33 +0530

    new update

--- src/app/attendance/service/emplyoee.service.ts
+++ src/app/attendance/service/emplyoee.service.ts
@@ -5,9 +5,10 @@ import { BehaviorSubject, Observable, Subject } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-//new
+
 export class EmplyoeeService {
   ApiPath: any = 'http://localhost:8093/api/'
+
   // ApiPath: any = 'https://entry-systemapinew.onrender.com/api/'
 
   employeeAttendance = new BehaviorSubject('');
