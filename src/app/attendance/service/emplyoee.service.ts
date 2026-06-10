8298a06f1967c108cf5c7f6e0629c13b6b8a3f8f
peehu sharma <videveloper101@yahoo.com>
2026-06-10 11:26:15 +0530

    newwwwcurrent

--- src/app/attendance/service/emplyoee.service.ts
+++ src/app/attendance/service/emplyoee.service.ts
@@ -7,9 +7,9 @@ import { BehaviorSubject, Observable, Subject } from 'rxjs';
 })
 
 export class EmplyoeeService {
-  ApiPath: any = 'http://localhost:8093/api/'
+  // ApiPath: any = 'http://localhost:8093/api/'
 
-  // ApiPath: any = 'https://entry-systemapinew.onrender.com/api/'
+  ApiPath: any = 'https://entry-systemapinew.onrender.com/api/'
 
   employeeAttendance = new BehaviorSubject('');
   disableEnterBtn = new Subject();
